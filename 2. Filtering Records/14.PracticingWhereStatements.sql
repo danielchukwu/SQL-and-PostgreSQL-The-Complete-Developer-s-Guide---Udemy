83c53266a46947c26240b79fdf0f3f0bd73f19d2
Daniel Chukwu <00danzy@gmail.com>
2022-08-29 21:29:10 +0100

    complete solution

---
+++ 2. Filtering Records/14.PracticingWhereStatements.sql
@@ -0,0 +1,10 @@
+-- SQL - ()
+-- Program: Using the below table called phones. Insert the table into our database
+-- name             manufacturer       Price         units_sold
+-- N1289            Nokia              199           1925
+-- Iphone 4         Apple              399           9436
+-- GalaxyS          Samsung            299           2359
+-- S5620            Samsung            250           2385 
+-- N8               Nokia              150           7543
+
+-- > Write a query that will print the name and price of all the phones that sold greater than 5000 units
