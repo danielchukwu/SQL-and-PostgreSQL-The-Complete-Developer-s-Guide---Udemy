3d662aeeb1336ce0e6d19e1000af10cd6ea1aa75
Daniel Chukwu <00danzy@gmail.com>
2022-08-29 12:17:59 +0100

    first commit

---
+++ 1. Simple But Powerful - SQL Statements/8-9. CalculatingPhoneRevenue.sql
@@ -0,0 +1,11 @@
+-- SQL - ()
+-- Program: Using the below table called phones. Insert the table into our database
+-- name             manufacturer       Price         units_sold
+-- N1289            Nokia              199           1925
+-- Iphone 4         Apple              399           9436
+-- GalaxyS          Samsung            299           2359
+-- S5620            Samsung            250           2385 
+-- N8               Nokia              150           7543
+
+-- > write a query that will select the name of each phone and calculate the total revenue for each phone (price * units_sold)
+-- > Rename this calculated column to revenue
