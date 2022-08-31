ad60a6f7490a52d4db232ee4db6f1e01a99ce3ca
Daniel Chukwu <00danzy@gmail.com>
2022-08-31 19:35:41 +0100

    complete little program

--- 4. Relating Records With Joins/46.MissingDataInJoins.sql
+++ 4. Relating Records With Joins/46.MissingDataInJoins.sql
@@ -11,3 +11,34 @@
 
 
 
+-- solution
+
+
+-- CREATE TABLE users (
+--   id SERIAL PRIMARY KEY,
+--   username VARCHAR(50)
+-- );
+
+-- INSERT INTO users (username)
+-- VALUES 
+--   ('Reyna.Marvin'),
+--   ('Micah.Cremin'),
+--   ('Alfredo66'),
+--   ('Gerald_Mitchell42'),
+--   ('Frederique_Donnelly');
+
+-- CREATE TABLE photos (
+--   id SERIAL PRIMARY KEY,
+--   url VARCHAR(200),
+--   user_id INTEGER REFERENCES users(id)
+-- );
+
+-- INSERT INTO photos (url, user_id)
+-- VALUES 
+--   ('https://santina.net', 2),
+--   ('https://alayna.net', 3),
+--   ('https://kaliyn.name', 1),
+--   ('https://banner.jpg', NULL);
+
+SELECT * FROM photos
+JOIN users ON users.id = photos.user_id
