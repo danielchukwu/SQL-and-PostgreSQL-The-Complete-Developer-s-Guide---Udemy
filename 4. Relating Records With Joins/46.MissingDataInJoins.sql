78b44dd211bb32f923179cce891699061d2aeaed
Daniel Chukwu <00danzy@gmail.com>
2022-08-31 19:34:35 +0100

    add problem

---
+++ 4. Relating Records With Joins/46.MissingDataInJoins.sql
@@ -0,0 +1,13 @@
+-- SQL - ()
+-- Program: Get all the photos in our database with the user that uploaded the photo
+
+--         users                          photos
+-- id      username            |   id     url                     author_id
+-- 1     Reyna.Marvin          |   1      http://santina.net         2
+-- 2     Micah.Cremin          |   2      http://alayna.net          3
+-- 3     Alfredo66             |   3      http://kaliyn.name         1
+-- 4     Gerald_Mitchell42     |   4      http://banner.jpg         null
+-- 5     Frederique_Donnelly   |
+
+
+
