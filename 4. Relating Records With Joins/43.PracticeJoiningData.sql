3bb04424c45d5ab87557cfcb1cc60b434bfdaf92
Daniel Chukwu <00danzy@gmail.com>
2022-08-31 17:25:02 +0100

    complete solution

---
+++ 4. Relating Records With Joins/43.PracticeJoiningData.sql
@@ -0,0 +1,13 @@
+-- SQL - ()
+-- Program: You are designing a database for a book publishing company. The database needs to store a table of authors and books. An author has many books. This means that books should have a foreign key column/property that references another
+
+--        authors                         books
+-- id      name                |   id     title                  author_id
+-- 1     JK Rowling            |   1      It                         2
+-- 2    Stephen King           |   2      Chamber of Secrets         1
+-- 3    Agatha Christie        |   3      Cat and the Hat            4
+-- 4     Dr Suess              |   4      Affair at Styles           3
+
+-- 1. Implement both tables and insert their individual records
+-- 2. Write a query that will join together these two tables. For each book, print the title of the book and the name of the author 
+
