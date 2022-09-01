c1aa40fce0810b028ab8848e9d9cf8197ab8f06c
Daniel Chukwu <00danzy@gmail.com>
2022-09-01 06:56:03 +0100

    add problem

---
+++ 4. Relating Records With Joins/51-52.ExerciseOverview.sql
@@ -0,0 +1,13 @@
+-- SQL - ()
+-- Program: You are still authoring a database for a book publisher. The database has a table of authors and a table of books
+
+--        authors                         books
+-- id      name                |   id     title                  author_id
+-- 1    Stephen King           |   1      The Dark Tower             1
+-- 2    Agatha Christie        |   2      Affair at Styles           3
+-- 3     JK Rowling            |   3      Murder At the Links        2
+
+-- Write a query that will return the title of each book, along with the name of the author. All authors should be included, even if they do not have a book associated with them.
+
+
+
