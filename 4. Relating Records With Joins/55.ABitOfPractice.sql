5f943ccbfba65b793fa8e07b7f2de0ca0fa67c2f
Daniel Chukwu <00danzy@gmail.com>
2022-09-01 21:02:51 +0100

    complete solution

---
+++ 4. Relating Records With Joins/55.ABitOfPractice.sql
@@ -0,0 +1,18 @@
+-- SQL - ()
+-- Program: Hmmm... i wonder if any authors are writing their own reviews for books!. You are working with a database books, authors, and reviews
+
+--        authors                         books                                           reviews   
+-- id      name            |   id     title                author_id    |   id   rating   reviewers_id    book_id
+-- 1    Stephen King       |   1      The Dark Tower           1        |   1      3           1             2
+-- 2    Agatha Christie    |   2      Affair at Styles         3        |   2      4           2             1
+-- 3     JK Rowling        |   3      Murder At the Links      2        |   3      5           3             3
+
+-- Write a query that will return the title of each book, along with the name of the author, and the rating of a review. Only show rows where the author of the book is also the author of the review
+
+
+
+
+-- solution
+
+
+
