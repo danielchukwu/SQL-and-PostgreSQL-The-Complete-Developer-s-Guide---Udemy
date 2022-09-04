1ebfaf276bae2485e782a0f73ee20b5cb4f7a99e
danielchukwu <00danzy@gmail.com>
2022-09-04 19:23:25 +0100

    complete solution

---
+++ 5. Aggregation Of Records/61.AGotchaWithCount.sql
@@ -0,0 +1,14 @@
+-- SQL - {}
+-- Program: What will be the result of the below code?
+
+-- SELECT user_id, COUNT(photo_id)
+-- FROM posts
+-- GROUP BY user_id
+
+-- for the below table posts
+
+-- id     url         user_id   photo_id
+-- 1    santina.net     3         null
+-- 2    alayna.net      5          7
+-- 3    kailyn.name     3         null
+
