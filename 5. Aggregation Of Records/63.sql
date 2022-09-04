75fd91cadea35de61acdaf8beb536ef7fd1fd73a
danielchukwu <00danzy@gmail.com>
2022-09-04 14:07:29 +0100

    completely add question

--- 5. Aggregation Of Records/63.sql
+++ 5. Aggregation Of Records/63.sql
@@ -1,1 +1,24 @@
+-- LESSON 65
+
+
+-- SQL - {}
+-- Program: This is the same exercise as the last one, but this time we want to print out some information from both tables!
+-- Write a query that will print an author's id and the number of books they have authored
+
+
+--      authors                    books
+-- id   name               \  id   title                 author_id
+-- 1    Jk Rowling         \  1    Chamber of Secrets    1
+-- 2    Stephen King       \  2    Prisoner of Azkaban   1
+-- 3    Agatha Christie    \  3    The Dark Tower        2
+-- 4    Dr Seus            \  4    Murder At the Links   3
+--                         \  5    Affair at Styles      3
+--                         \  6    Cat in the Hat        4
+
+-- 1  chamber
+--    prisoner
+-- 2  The dark
+-- 3  Murder at
+--    Affair at
+-- 4  Cat in the 
 
