47a258dc862b282a0f83cd0b0056d9dc7fc925be
danielchukwu <00danzy@gmail.com>
2022-09-05 21:34:44 +0100

    add question

---
+++ 6. Working With Large Dataset/75.InneroinReview.sql
@@ -0,0 +1,20 @@
+-- SQL - {}
+-- Program: Join together the users and orders tables. Print the first_name and last_name of each user,
+            -- then whether or not they have paid for their orders
+
+--           orders
+-- id  user_id   product_id   paid
+-- 1      5          4        true
+-- 2      3          2        true
+-- 3      4          6        false
+-- 4      5          3        true
+-- 5      1          1        false
+
+-- show diagramatic process of this on a sheet of paper before writing any SQL
+
+
+
+
+-- solution
+
+
