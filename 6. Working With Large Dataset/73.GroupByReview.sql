f73b4faf8fe4a67f945a826bf38af340b88d609c
danielchukwu <00danzy@gmail.com>
2022-09-05 21:29:35 +0100

    first commit

---
+++ 6. Working With Large Dataset/73.GroupByReview.sql
@@ -0,0 +1,10 @@
+-- SQL - {}
+-- Program: Write a query to print the number of paid and unpaid orders
+
+--           orders
+-- id  user_id   product_id   paid
+-- 1      5          4        true
+-- 2      3          2        true
+-- 3      4          6        false
+-- 4      5          3        true
+-- 5      1          1        false
