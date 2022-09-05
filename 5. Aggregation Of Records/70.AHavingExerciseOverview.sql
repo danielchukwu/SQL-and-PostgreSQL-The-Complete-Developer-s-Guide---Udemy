a83fd402071437f6db57d173f35eee7eedf2ab62
danielchukwu <00danzy@gmail.com>
2022-09-05 19:08:25 +0100

    set question

---
+++ 5. Aggregation Of Records/70.AHavingExerciseOverview.sql
@@ -0,0 +1,12 @@
+SQL - {}
+-- Program: You are trying to find the phone manufacturers with the greatest revenue from selliing phones.
+
+-- name             manufacturer       Price         units_sold
+-- N1289            Nokia              199           1925
+-- Iphone 4         Apple              399           9436
+-- GalaxyS          Samsung            299           2359
+-- S5620            Samsung            250           2385 
+-- N8               Nokia              150           7543
+
+-- Given a table of phones, print the names of manufacturers and total revenue (price * units_sold) for all phones.
+-- Only print the manufacturers who have reveue greater than 2,000,000
