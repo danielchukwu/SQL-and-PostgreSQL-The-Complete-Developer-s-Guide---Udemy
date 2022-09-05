a91ac3fcdc184e56090b049e423fa3b73d54b2d5
danielchukwu <00danzy@gmail.com>
2022-09-05 19:36:38 +0100

    complete solution

--- 5. Aggregation Of Records/70.AHavingExerciseOverview.sql
+++ 5. Aggregation Of Records/70.AHavingExerciseOverview.sql
@@ -1,4 +1,4 @@
-SQL - {}
+-- SQL - {}
 -- Program: You are trying to find the phone manufacturers with the greatest revenue from selliing phones.
 
 -- name             manufacturer       Price         units_sold
@@ -10,3 +10,35 @@ SQL - {}
 
 -- Given a table of phones, print the names of manufacturers and total revenue (price * units_sold) for all phones.
 -- Only print the manufacturers who have reveue greater than 2,000,000
+
+
+
+
+-- solution
+
+
+
+
+-- step 1
+-- CREATE TABLE phones(
+--   name VARCHAR(50),
+--   manufacturer VARCHAR(50),
+--   price INTEGER,
+--   units_sold INTEGER
+-- );
+
+-- step 2
+-- INSERT INTO phones (name, manufacturer, price, units_sold)
+-- VALUES
+--   ('N1280', 'Nokia', 199, 1925),
+--   ('Iphone 4', 'Apple', 399, 9436),
+--   ('GalaxyS', 'Samsung', 299, 2359),
+--   ('S5620', 'Samsung', 250, 2385),
+--   ('N8', 'Nokia', 150, 7543);
+
+
+
+SELECT manufacturer, SUM(price) * SUM(units_sold) AS total_revenue
+FROM phones
+GROUP BY manufacturer
+HAVING SUM(price) * SUM(units_sold) > 2000000
