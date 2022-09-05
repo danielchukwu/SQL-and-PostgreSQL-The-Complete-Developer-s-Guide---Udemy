217238ce9076accfa9e7663a931fde812d4a2c69
danielchukwu <00danzy@gmail.com>
2022-09-05 21:38:29 +0100

    add another table

--- 6. Working With Large Dataset/75.InnerJoinReview.sql
+++ 6. Working With Large Dataset/75.InnerJoinReview.sql
@@ -1,6 +1,6 @@
 -- SQL - {}
 -- Program: Join together the users and orders tables. Print the first_name and last_name of each user,
-            -- then whether or not they have paid for their orders
+--          then whether or not they have paid for their orders
 
 --           orders
 -- id  user_id   product_id   paid
@@ -9,6 +9,13 @@
 -- 3      4          6        false
 -- 4      5          3        true
 -- 5      1          1        false
+
+-- id  first_name  last_name
+-- 1      iva       lindgren
+-- 2    ignatius     johns
+-- 3     jannie      boehm
+-- 4      neal       Wehner
+-- 5    Mikayla      Casper
 
 -- show diagramatic process of this on a sheet of paper before writing any SQL
 
