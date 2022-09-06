7c9c40158ac19f595a5e4c22988ba0400d6c7804
danielchukwu <00danzy@gmail.com>
2022-09-06 14:51:26 +0100

    create table

--- 9. Assembling Queries with Subqueries/91.EmbeddingInSelect.sql
+++ 9. Assembling Queries with Subqueries/91.EmbeddingInSelect.sql
@@ -10,3 +10,24 @@
 -- GalaxyS          Samsung            299           2359
 -- S5620            Samsung            250           2385 
 -- N8               Nokia              150           7543
+
+
+-- solution
+
+
+-- step 1
+-- CREATE TABLE phones(
+--   name VARCHAR(50),
+--   manufacturer VARCHAR(50),
+--   price INTEGER,
+--   units_sold INTEGER
+-- );
+
+-- step 2
+-- INSERT INTO phones (name, manufacturer, price, units_sold)
+-- VALUES
+--   ('N1280', 'Nokia', 199, 1925),
+--   ('Iphone 4', 'Apple', 399, 9436),
+--   ('GalaxyS', 'Samsung', 299, 2359),
+--   ('S5620', 'Samsung', 250, 2385),
+--   ('N8', 'Nokia', 150, 7543);
