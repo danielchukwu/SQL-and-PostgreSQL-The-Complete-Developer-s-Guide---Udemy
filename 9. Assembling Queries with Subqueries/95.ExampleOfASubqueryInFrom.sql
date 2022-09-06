82d68cddab99eef670ef8c173a7236d694d7414a
danielchukwu <00danzy@gmail.com>
2022-09-06 19:27:13 +0100

    add table for problem

--- 9. Assembling Queries with Subqueries/95.ExampleOfASubqueryInFrom.sql
+++ 9. Assembling Queries with Subqueries/95.ExampleOfASubqueryInFrom.sql
@@ -1,3 +1,11 @@
-SQL - {}
-Program: Find the average number of orders for all users in the below orders table
+-- SQL - {}
+-- Program: Find the average number of orders for all users in the below orders table
 
+-- id  user_id   product_id   paid
+-- 1      5          4        true
+-- 2      3          2        true
+-- 3      4          6        false
+-- 4      5          3        true
+-- 5      1          1        false
+-- 6      5          3        true
+-- 7      1          1        false
