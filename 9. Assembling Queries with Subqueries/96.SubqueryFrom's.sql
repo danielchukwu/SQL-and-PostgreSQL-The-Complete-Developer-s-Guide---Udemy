e0e8c80690f214a221a83e2bedfc25d380829b66
danielchukwu <00danzy@gmail.com>
2022-09-06 19:52:23 +0100

    add table for problem

--- 9. Assembling Queries with Subqueries/96.SubqueryFrom's.sql
+++ 9. Assembling Queries with Subqueries/96.SubqueryFrom's.sql
@@ -1,3 +1,10 @@
-SQL - {}
-Program: calculate the average price of phones for each manufacturer. Then print the highest average price
+-- SQL - {}
+-- Program: calculate the average price of phones for each manufacturer. Then print the highest average price
 
+
+-- name             manufacturer       Price         units_sold
+-- N1289            Nokia              199           1925
+-- Iphone 4         Apple              399           9436
+-- GalaxyS          Samsung            299           2359
+-- S5620            Samsung            250           2385 
+-- N8               Nokia              150           7543
