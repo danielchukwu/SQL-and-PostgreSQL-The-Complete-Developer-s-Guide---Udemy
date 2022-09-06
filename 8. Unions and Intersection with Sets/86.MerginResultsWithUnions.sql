61d28a7eed7334536e9114faf4c6ba808bf8bebe
danielchukwu <00danzy@gmail.com>
2022-09-06 07:25:00 +0100

    completely add question

---
+++ 8. Unions and Intersection with Sets/86.MerginResultsWithUnions.sql
@@ -0,0 +1,16 @@
+-- SQL - {}
+-- program: Write a query that will print the manufacturers of phones where the phone's price is less than 170. 
+--          Also print all manufacturer that have created more than two phones
+
+
+-- name             manufacturer       Price         units_sold
+-- N1289            Nokia              199           1925
+-- Iphone 4         Apple              399           9436
+-- GalaxyS          Samsung            299           2359
+-- S5620            Samsung            250           2385 
+-- N8               Nokia              150           7543
+
+
+
+-- solution
+
