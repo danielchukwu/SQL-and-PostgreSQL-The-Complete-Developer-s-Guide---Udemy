890636efb092439236591bfd0272b3ee2f42afbd
danielchukwu <00danzy@gmail.com>
2022-09-06 14:49:56 +0100

    add table structure for problem

--- 9. Assembling Queries with Subqueries/91.EmbeddingInSelect.sql
+++ 9. Assembling Queries with Subqueries/91.EmbeddingInSelect.sql
@@ -2,3 +2,11 @@
 -- Program: Write a query that prints the name and price for each phone. In addition, print
 --          out the ratio of the phones price against max price of all phones. Rename 
 --          this third column to price_ratio
+
+
+-- name             manufacturer       Price         units_sold
+-- N1289            Nokia              199           1925
+-- Iphone 4         Apple              399           9436
+-- GalaxyS          Samsung            299           2359
+-- S5620            Samsung            250           2385 
+-- N8               Nokia              150           7543
