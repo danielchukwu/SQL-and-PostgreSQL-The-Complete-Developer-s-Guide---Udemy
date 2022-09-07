088ae0c3b41d023b846347d0dfbae555373e54a5
danielchukwu <00danzy@gmail.com>
2022-09-07 10:14:10 +0100

    add table

--- 9. Assembling Queries with Subqueries/104.ANewOperator.sql
+++ 9. Assembling Queries with Subqueries/104.ANewOperator.sql
@@ -1,3 +1,15 @@
 -- SQL - {}
 -- problem: show the name of all products with a price greater than 
 --          the average product price
+
+
+--              products
+-- id   name     name        price  weight
+-- 1    shirt    Toys        876    3
+-- 2    Towels   Outdoors    412    16
+-- 3    Bacon    Movies      10     6
+-- 4    Ball     Industrial  328    23
+-- 5    Fish     Tools       796    10
+-- 6    Mouse    Grocery     989    11
+-- 7    Computer Home        298    2
+
