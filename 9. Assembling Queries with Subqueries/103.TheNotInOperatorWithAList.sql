6d5ff878dc329822ed6b197cf7109a259c891efa
danielchukwu <00danzy@gmail.com>
2022-09-07 10:13:01 +0100

    complete solution

--- 9. Assembling Queries with Subqueries/103.TheNotInOperatorWithAList.sql
+++ 9. Assembling Queries with Subqueries/103.TheNotInOperatorWithAList.sql
@@ -15,3 +15,13 @@
 
 
 
+-- solution
+
+
+SELECT name
+FROM products
+WHERE department NOT IN (
+  SELECT department FROM products WHERE price < 100
+);
+
+-- SELECT department FROM products WHERE price < 100
