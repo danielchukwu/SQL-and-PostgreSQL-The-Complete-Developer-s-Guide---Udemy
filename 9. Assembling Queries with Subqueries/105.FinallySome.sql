1ec1935139dc6379e367c16691d95ad33ad247a9
danielchukwu <00danzy@gmail.com>
2022-09-07 11:27:52 +0100

    add table

--- 9. Assembling Queries with Subqueries/105.FinallySome.sql
+++ 9. Assembling Queries with Subqueries/105.FinallySome.sql
@@ -2,3 +2,13 @@
 -- problem: show the name of all products that are more expensive than atleast
 --          one product in the 'Industrial' department
 
+
+--              products
+-- id   name     name        price  weight
+-- 1    shirt    Toys        876    3
+-- 2    Towels   Outdoors    412    16
+-- 3    Bacon    Movies      10     6
+-- 4    Ball     Industrial  328    23
+-- 5    Fish     Tools       796    10
+-- 6    Mouse    Grocery     989    11
+-- 7    Computer Home        298    2
