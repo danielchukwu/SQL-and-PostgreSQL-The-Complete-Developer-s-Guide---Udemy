ab9d01be07488d34eb3c3623de9320a3c3f1aad1
danielchukwu <00danzy@gmail.com>
2022-09-07 10:12:40 +0100

    add table

--- 9. Assembling Queries with Subqueries/103.TheNotInOperatorWithAList.sql
+++ 9. Assembling Queries with Subqueries/103.TheNotInOperatorWithAList.sql
@@ -2,3 +2,16 @@
 -- problem: show the name of all products that are not in the same department
 --          as products with a price less than 100
 
+
+--              products
+-- id   name     name        price  weight
+-- 1    shirt    Toys        876    3
+-- 2    Towels   Outdoors    412    16
+-- 3    Bacon    Movies      10     6
+-- 4    Ball     Industrial  328    23
+-- 5    Fish     Tools       796    10
+-- 6    Mouse    Grocery     989    11
+-- 7    Computer Home        298    2
+
+
+
