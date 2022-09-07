8b2e694c5f9f463b7c3db1bcd36721997e7128ea
danielchukwu <00danzy@gmail.com>
2022-09-07 16:37:21 +0100

    minor changes

--- 9. Assembling Queries with Subqueries/108.ProbablyTooMuchAboutCorrelatedSubqueries.sql
+++ 9. Assembling Queries with Subqueries/108.ProbablyTooMuchAboutCorrelatedSubqueries.sql
@@ -1,14 +1,18 @@
 -- SQL - {}
--- problem: show the name, department, and price of products that are more expensive
---          than all products in the industrial department
+-- problem: show the name, department, and price of the most expensive product in each department
 
 
 --              products
--- id   name     name        price  weight
--- 1    shirt    Toys        876    3
+-- id   name     department  price  weight
+-- 1    shirt    Industrial  876    3
 -- 2    Towels   Outdoors    412    16
--- 3    Bacon    Movies      10     6
+-- 3    Bacon    Grocery     10     6
 -- 4    Ball     Industrial  328    23
--- 5    Fish     Tools       796    10
+-- 5    Fish     Industrial  796    10
 -- 6    Mouse    Grocery     989    11
--- 7    Computer Home        298    2
+-- 7    Computer Outdoors    298    2
+
+
+
+
+-- solution
