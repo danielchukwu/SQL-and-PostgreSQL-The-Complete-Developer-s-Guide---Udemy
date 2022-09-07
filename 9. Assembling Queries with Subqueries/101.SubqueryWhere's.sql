41668837ec470938d4ad7a7e20de8f392ff21948
danielchukwu <00danzy@gmail.com>
2022-09-07 08:28:34 +0100

    add table

--- 9. Assembling Queries with Subqueries/101.SubqueryWhere's.sql
+++ 9. Assembling Queries with Subqueries/101.SubqueryWhere's.sql
@@ -1,3 +1,16 @@
 -- SQL - {}
 -- problem: Write a query that prints out the name and price of phones that have
 --          a price greater than the Samsung S5620
+
+--                          phones
+-- name             manufacturer       Price         units_sold
+-- N1289            Nokia              199           1925
+-- Iphone 4         Apple              399           9436
+-- GalaxyS          Samsung            299           2359
+-- S5620            Samsung            250           2385 
+-- N8               Nokia              150           7543
+
+
+
+
+
