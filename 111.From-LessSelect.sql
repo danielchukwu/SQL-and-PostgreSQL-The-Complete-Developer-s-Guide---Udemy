45da2b900e72d2dd6703b89d69500ddac063f06b
danielchukwu <00danzy@gmail.com>
2022-09-08 19:42:13 +0100

    add table

--- 111.From-LessSelect.sql
+++ 111.From-LessSelect.sql
@@ -2,3 +2,11 @@
 -- Program: Using only subqueries, price, min price, and average price of all phones.
 --          Rename each column to max_price, min_price, avg_price
 
+
+-- name             manufacturer       Price         units_sold
+-- N1289            Nokia              199           1925
+-- Iphone 4         Apple              399           9436
+-- GalaxyS          Samsung            299           2359
+-- S5620            Samsung            250           2385 
+-- N8               Nokia              150           7543
+
