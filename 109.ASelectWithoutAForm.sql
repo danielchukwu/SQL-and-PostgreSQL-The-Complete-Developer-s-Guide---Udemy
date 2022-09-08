027d55b171cab5a2328f555363710a6e445ee1bc
danielchukwu <00danzy@gmail.com>
2022-09-08 19:40:28 +0100

    add table

--- 109.ASelectWithoutAForm.sql
+++ 109.ASelectWithoutAForm.sql
@@ -1,3 +1,14 @@
 -- SQL - {}
 -- problem: without using a join or a group by, print the number of orders for each product
 
+
+--              products                      |             orders
+-- id   name     department  price  weight    | id  user_id  product_id  paid
+-- 1    shirt    Industrial  876    3         | 1     5          4       true
+-- 2    Towels   Outdoors    412    16        | 2     3          2       true
+-- 3    Bacon    Grocery     10     6         | 3     4          6       false
+-- 4    Ball     Industrial  328    23        | 4     5          3       true
+-- 5    Fish     Industrial  796    10        | 5     1          1       false
+-- 6    Mouse    Grocery     989    11        |
+-- 7    Computer Outdoors    298    2         |
+
