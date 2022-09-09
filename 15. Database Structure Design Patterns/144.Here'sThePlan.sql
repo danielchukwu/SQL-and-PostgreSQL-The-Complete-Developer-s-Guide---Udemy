8fd3e311b8a93fd80f6de7577b79c61b652db2ce
danielchukwu <00danzy@gmail.com>
2022-09-09 15:12:45 +0100

    add structure of tables that is required to be built

--- 15. Database Structure Design Patterns/144.Here'sThePlan.sql
+++ 15. Database Structure Design Patterns/144.Here'sThePlan.sql
@@ -1,2 +1,11 @@
 --SQL - {}
 --Program: using dbdiagram.io/d/ website to create all 3 tables (users, posts, comments) for our instagram database application
+
+
+-- Table users  |Table posts  |Table comments
+-- id           |id           |id
+-- updated_at   |updated_at   |updated_at
+-- created_at   |created_at   |created_at
+-- username     |url          |text
+-- email        |user_id      |user_id
+--                            |post_id
