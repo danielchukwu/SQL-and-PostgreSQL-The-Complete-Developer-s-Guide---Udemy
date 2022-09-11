53d16852fcd4aa1781e88d3a7efc88fce844a849
danielchukwu <00danzy@gmail.com>
2022-09-11 10:42:42 +0100

    create users table

--- 17. How To Build A 'Mention' System/161.CompleteCreationOfInstagramDatabaseDesign.sql
+++ 17. How To Build A 'Mention' System/161.CompleteCreationOfInstagramDatabaseDesign.sql
@@ -1,3 +1,17 @@
 -- SQL - {}
 -- program: analyze instagram and step by step build a complete database design for the features you notice
 
+
+
+-- solution
+
+
+
+-- TABLE users {
+--   id integer [pk, increment]
+--   username varchar(50)
+--   email varchar(50)
+  
+--   updated_at timestamp
+--   created_at timestamp
+-- }
