b145be714ca1b0e155d51a071d492e1f9c3fe574
danielchukwu <00danzy@gmail.com>
2022-09-12 06:43:36 +0100

    add users and likes table

--- 21. Approaching and Writing Complex Queries/175.PostsByAParticularUser.sql
+++ 21. Approaching and Writing Complex Queries/175.PostsByAParticularUser.sql
@@ -1,3 +1,14 @@
 -- SQL - {}
 -- problem: show each username and the number of 'likes' that they have CREATED
 
+
+--             users                   |         likes
+-- id   username   password   status   |id   caption    user_id
+-- 1    gai81      LKJ223j5ij online   |1    #ominis    203
+-- 2    eise20     null       offline  |2    quas cosq  200
+-- 3    james80    null       offline  |3    #mary      201
+-- 4    jon37      ikp5153    online   |4    officis    202
+-- 5    neva36     aijg9225   online   |5    #generate  200
+-- 6    Ramona92   null       online   |6    #content   199
+
+
