29a61cfec761a5dd639e9766c12c3bf2c73dc9b7
danielchukwu <00danzy@gmail.com>
2022-09-12 06:21:05 +0100

    create hashtags_posts table

--- 20. Implementing Database Design Patterns/165-170.ImplementingDatabaseDesignPattern.sql
+++ 20. Implementing Database Design Patterns/165-170.ImplementingDatabaseDesignPattern.sql
@@ -107,3 +107,12 @@ CREATE TABLE hashtags (
 	
 	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE hashtags_posts (
+	id SERIAL PRIMARY KEY,
+	hashtag_id INTEGER NOT NULL REFERENCES hashtags(id) ON DELETE CASCADE,
+	post_id INTEGER NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
+	
+	UNIQUE (hashtag_id, post_id)
+);
+
