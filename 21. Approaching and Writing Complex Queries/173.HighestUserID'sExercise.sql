52b7bc13a0d0701eb3f839875354e4f6bba235ed
danielchukwu <00danzy@gmail.com>
2022-09-12 06:29:53 +0100

    add table

--- 21. Approaching and Writing Complex Queries/173.HighestUserID'sExercise.sql
+++ 21. Approaching and Writing Complex Queries/173.HighestUserID'sExercise.sql
@@ -1,2 +1,15 @@
 -- SQL - {}
 -- problem: select the 3 users with the highest ID's from the users table
+
+
+--             users
+-- id   username   password   status
+-- 1    gai81      LKJ223j5ij online
+-- 2    eise20     null       offline
+-- 3    james80    null       offline
+-- 4    jon37      ikp5153    online
+-- 5    neva36     aijg9225   online
+-- 6    Ramona92   null       online
+
+
+
