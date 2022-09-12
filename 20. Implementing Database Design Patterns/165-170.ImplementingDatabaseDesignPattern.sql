eafbab3920e3949ba25709b80b22c86977c28f04
danielchukwu <00danzy@gmail.com>
2022-09-12 06:18:36 +0100

    create users table

--- 20. Implementing Database Design Patterns/165-170.ImplementingDatabaseDesignPattern.sql
+++ 20. Implementing Database Design Patterns/165-170.ImplementingDatabaseDesignPattern.sql
@@ -1,3 +1,27 @@
 -- SQL - {}
 -- goal: implement design pattern for your instagram application on a new database you create -> called instagram. using pgAdmin
 
+
+
+-- solution
+
+
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	username VARCHAR(30) NOT NULL UNIQUE,
+	bio VARCHAR(400),
+	avatar VARCHAR(200),
+	phone VARCHAR(25),
+	email VARCHAR(40),
+	password VARCHAR(50),
+	status VARCHAR(15),
+	
+	CHECK (COALESCE(phone, email) IS NOT NULL),
+	CHECK (
+		(phone IS NOT NULL AND password IS NOT NULL) OR 
+		(email IS NOT NULL AND password IS NOT NULL)
+	),
+	
+	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
