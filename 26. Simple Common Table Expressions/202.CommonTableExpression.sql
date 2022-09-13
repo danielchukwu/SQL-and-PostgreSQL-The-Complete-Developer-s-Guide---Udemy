15e7a8b70854d22f756b1886b5a190e890ccec8a
danielchukwu <00danzy@gmail.com>
2022-09-13 17:32:04 +0100

    complete solution 2

--- 26. Simple Common Table Expressions/202.CommonTableExpression.sql
+++ 26. Simple Common Table Expressions/202.CommonTableExpression.sql
@@ -34,4 +34,8 @@ WITH tags AS (
    UNION ALL
    SELECT user_id, created_at FROM photo_tags
 )
+SELECT username, tags.created_at
+FROM users
+JOIN tags ON tags.user_id = users.id
+WHERE tags.created_at < '2010-01-07';
 
