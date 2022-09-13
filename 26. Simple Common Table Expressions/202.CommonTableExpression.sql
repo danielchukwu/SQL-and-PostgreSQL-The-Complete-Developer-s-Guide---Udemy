5a3f6bfa85fa0d96163c96194e067208ba60db77
danielchukwu <00danzy@gmail.com>
2022-09-13 17:30:07 +0100

    add exercise 2

--- 26. Simple Common Table Expressions/202.CommonTableExpression.sql
+++ 26. Simple Common Table Expressions/202.CommonTableExpression.sql
@@ -24,3 +24,8 @@ JOIN (
    SELECT user_id, created_at FROM photo_tags
 ) AS tags ON tags.user_id = users.id
 WHERE tags.created_at < '2010-01-07';
+
+
+
+
+-- Exercise2: Improve this using simple common table expression
