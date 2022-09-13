cbeb4e2996e8a71a0dc604d46fd5bf568dc10896
danielchukwu <00danzy@gmail.com>
2022-09-13 17:29:25 +0100

    complete solution

--- 26. Simple Common Table Expressions/202.CommonTableExpression.sql
+++ 26. Simple Common Table Expressions/202.CommonTableExpression.sql
@@ -4,10 +4,23 @@
 
 
 --    users     |       caption_tags         |        photo_tags
-id   username   |id   user_id   created_at   |id   user_id   created_at
-10   Hallie     |1       1      2014-01-01   |1       5      2013-02-06
-11   Eldon      |2       4      2010-01-05   |2       2      2017-03-28
-12   Rozella    |3       2      2012-11-04   |3       7      2016-02-09
-13   Tad        |4       1      2015-04-07   |4       5      2010-03-06
-14   Kim        |5       5      2010-01-01   |5       2      2010-01-03
-15   Daniella   |6       4      2019-10-10   |6       1      2010-12-27
+-- id   username   |id   user_id   created_at   |id   user_id   created_at
+-- 10   Hallie     |1       1      2014-01-01   |1       5      2013-02-06
+-- 11   Eldon      |2       4      2010-01-05   |2       2      2017-03-28
+-- 12   Rozella    |3       2      2012-11-04   |3       7      2016-02-09
+-- 13   Tad        |4       1      2015-04-07   |4       5      2010-03-06
+-- 14   Kim        |5       5      2010-01-01   |5       2      2010-01-03
+-- 15   Daniella   |6       4      2019-10-10   |6       1      2010-12-27
+
+
+-- solution
+
+
+SELECT username, tags.created_at
+FROM users
+JOIN (
+   SELECT user_id, created_at FROM caption_tags
+   UNION ALL
+   SELECT user_id, created_at FROM photo_tags
+) AS tags ON tags.user_id = users.id
+WHERE tags.created_at < '2010-01-07';
