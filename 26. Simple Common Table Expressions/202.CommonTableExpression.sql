1954f5c4738f37aefd8b4e770c10f401dc98c3b5
danielchukwu <00danzy@gmail.com>
2022-09-13 17:25:54 +0100

    add users table

--- 26. Simple Common Table Expressions/202.CommonTableExpression.sql
+++ 26. Simple Common Table Expressions/202.CommonTableExpression.sql
@@ -1,3 +1,13 @@
 -- SQL - {}
 -- Exercise: Show the username of users who where tagged in a caption or photo befor january 7th 2010. 
 -- Also show the date they were tagged
+
+
+--    users     | 
+id   username   |
+10   Hallie     |
+11   Eldon      |
+12   Rozella    |
+13   Tad        |
+14   Kim        |
+15   Daniella   |
