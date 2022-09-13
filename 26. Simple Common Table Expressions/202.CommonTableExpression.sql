e2eef08493b5aed0f8b04b027151b03be670d19a
danielchukwu <00danzy@gmail.com>
2022-09-13 17:26:15 +0100

    add caption_tags table

--- 26. Simple Common Table Expressions/202.CommonTableExpression.sql
+++ 26. Simple Common Table Expressions/202.CommonTableExpression.sql
@@ -3,11 +3,11 @@
 -- Also show the date they were tagged
 
 
---    users     | 
-id   username   |
-10   Hallie     |
-11   Eldon      |
-12   Rozella    |
-13   Tad        |
-14   Kim        |
-15   Daniella   |
+--    users     |       caption_tags         |
+id   username   |id   user_id   created_at   |
+10   Hallie     |1       1      2014-01-01   |
+11   Eldon      |2       4      2010-01-05   |
+12   Rozella    |3       2      2016-02-09   |
+13   Tad        |4       1      2010-03-06   |
+14   Kim        |5       5      2010-01-03   |
+15   Daniella   |6       4      2010-02-27   |
