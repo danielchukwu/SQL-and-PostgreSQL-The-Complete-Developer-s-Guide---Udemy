78f500e7876b2e6063bc0a831608ece009d10bbb
danielchukwu <00danzy@gmail.com>
2022-09-14 15:53:51 +0100

    add caption_tags table

--- 28. Simplifying Queries With Values/210.MostPopularUsers.sql
+++ 28. Simplifying Queries With Values/210.MostPopularUsers.sql
@@ -2,12 +2,12 @@ SQL -{}
 Exercise: Show the most popular users - the users who were tagged the most 
 
 
---        users          |      
--- id   username         |id   user_id
--- 1    Hallie           |1       5
--- 2    Justin Bieber    |2       6
--- 3    Jennifer Lopez   |3       7
--- 4    The Rock         |4       4
--- 5    Kevin Hart       |5       2
--- 6    Snoop Dog        |6       3
+--        users          | caption_tags    |
+-- id   username         |id   user_id     |
+-- 1    Hallie           |1       1        |
+-- 2    Justin Bieber    |2       4        |
+-- 3    Jennifer Lopez   |3       2        |
+-- 4    The Rock         |4       1        |
+-- 5    Kevin Hart       |5       5        |
+-- 6    Snoop Dog        |6       4        |
 
