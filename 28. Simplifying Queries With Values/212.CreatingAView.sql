92eb8f70d6dd1b4860bfd247e3bb48097c05495e
danielchukwu <00danzy@gmail.com>
2022-09-14 19:31:24 +0100

    add photo_tags table

--- 28. Simplifying Queries With Values/212.CreatingAView.sql
+++ 28. Simplifying Queries With Values/212.CreatingAView.sql
@@ -1,7 +1,7 @@
 -- SQL - {}
 -- Exercise: Create a view named tag by combining photo_tags and caption_tags
 
---  caption_tags
+--  caption_tags    | photo_tags
 -- id   user_id     |id   user_id
 -- 1       1        |1       5
 -- 2       4        |2       2
