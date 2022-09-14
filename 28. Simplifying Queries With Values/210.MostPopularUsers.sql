3c82021fb385009b29a99e5323a928931e49ff3e
danielchukwu <00danzy@gmail.com>
2022-09-14 15:54:52 +0100

    add photo_tags table

--- 28. Simplifying Queries With Values/210.MostPopularUsers.sql
+++ 28. Simplifying Queries With Values/210.MostPopularUsers.sql
@@ -2,12 +2,12 @@ SQL -{}
 Exercise: Show the most popular users - the users who were tagged the most 
 
 
---        users          | caption_tags    |
--- id   username         |id   user_id     |
--- 1    Hallie           |1       1        |
--- 2    Justin Bieber    |2       4        |
--- 3    Jennifer Lopez   |3       2        |
--- 4    The Rock         |4       1        |
--- 5    Kevin Hart       |5       5        |
--- 6    Snoop Dog        |6       4        |
+--        users          | caption_tags    | photo_tags
+-- id   username         |id   user_id     |id   user_id
+-- 1    Hallie           |1       1        |1       5
+-- 2    Justin Bieber    |2       4        |2       2
+-- 3    Jennifer Lopez   |3       2        |3       7
+-- 4    The Rock         |4       1        |4       5
+-- 5    Kevin Hart       |5       5        |5       2
+-- 6    Snoop Dog        |6       4        |6       1
 
