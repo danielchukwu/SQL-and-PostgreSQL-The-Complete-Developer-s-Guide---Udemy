0ff00190b1d36bd73e4270900fad86e33681d497
danielchukwu <00danzy@gmail.com>
2022-09-14 15:52:37 +0100

    add users table

--- 28. Simplifying Queries With Values/210.MostPopularUsers.sql
+++ 28. Simplifying Queries With Values/210.MostPopularUsers.sql
@@ -1,2 +1,13 @@
 SQL -{}
 Exercise: Show the most popular users - the users who were tagged the most 
+
+
+--        users          |      
+-- id   username         |id   user_id
+-- 1    Hallie           |1       5
+-- 2    Justin Bieber    |2       6
+-- 3    Jennifer Lopez   |3       7
+-- 4    The Rock         |4       4
+-- 5    Kevin Hart       |5       2
+-- 6    Snoop Dog        |6       3
+
