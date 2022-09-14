94915f391e7a1ea8235f7b8c8e8342ad772430bb
danielchukwu <00danzy@gmail.com>
2022-09-14 15:47:09 +0100

    add followers table

--- 26. Simple Common Table Expressions/208.WritingTheRecursiveQuery.sql
+++ 26. Simple Common Table Expressions/208.WritingTheRecursiveQuery.sql
@@ -1,7 +1,7 @@
 SQL - {}
 Exercise: Write a Recursive CTE query to suggest users at depth 2 for a particular user with id=1
 
---        users
+--        users                   followers
 id   username         |id   leader_id    follower_id
 1    Hallie           |1        5             1
 2    Justin Bieber    |2        6             5
