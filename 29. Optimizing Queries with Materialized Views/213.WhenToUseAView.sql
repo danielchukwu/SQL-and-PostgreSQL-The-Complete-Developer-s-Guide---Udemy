a8de2726cbc86d982126422aa2f7b63f00ef979f
danielchukwu <00danzy@gmail.com>
2022-09-16 07:21:05 +0100

    create view

--- 29. Optimizing Queries with Materialized Views/213.WhenToUseAView.sql
+++ 29. Optimizing Queries with Materialized Views/213.WhenToUseAView.sql
@@ -1,3 +1,14 @@
 -- SQL - {}
 -- Exercise: Create a view that contains top 10 most recent posts and then answer all
 --           6 questions below using the created view
+
+
+
+-- solution
+
+-- CREATE VIEW recent_posts AS (
+-- 	SELECT *
+-- 	FROM posts
+-- 	ORDER BY created_at DESC
+-- 	LIMIT 10
+-- )
