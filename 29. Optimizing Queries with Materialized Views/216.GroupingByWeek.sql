9e7dfca31308ab52d23ad76624b79df8a51dd3e0
danielchukwu <00danzy@gmail.com>
2022-09-16 07:24:46 +0100

    add posts table

--- 29. Optimizing Queries with Materialized Views/216.GroupingByWeek.sql
+++ 29. Optimizing Queries with Materialized Views/216.GroupingByWeek.sql
@@ -1,3 +1,14 @@
 -- SQL - {}
 -- Exercise: For each week, show the number of likes that posts and comments recieved. 
 --           Use the post and comment created_at date, not when the like was recieved
+
+--      posts                 
+-- created_at   id   |post_id   comment_id   |id   created_at
+-- 2010-01-01   1    |  2         null       |1    2010-01-03
+-- 2010-01-04   2    | null        3         |2    2010-01-09
+-- 2010-01-06   3    | null        2         |3    2010-01-010
+--                   |  2         null       |
+--                   |  2         null       |
+--                   | null        1         |
+
+
