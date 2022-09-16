edd2add3041163bdf165f67b16dadee0fa743eca
danielchukwu <00danzy@gmail.com>
2022-09-16 07:24:19 +0100

    set questions

---
+++ 29. Optimizing Queries with Materialized Views/216.GroupingByWeek.sql
@@ -0,0 +1,3 @@
+-- SQL - {}
+-- Exercise: For each week, show the number of likes that posts and comments recieved. 
+--           Use the post and comment created_at date, not when the like was recieved
