bce639e4820d8d36e8ae294619ac99e3cda1459a
danielchukwu <00danzy@gmail.com>
2022-09-16 18:56:32 +0100

    complete table-comments migration

--- 31/ig/migrations/1663331419191_table-comments.js
+++ 31/ig/migrations/1663331419191_table-comments.js
@@ -2,6 +2,19 @@
 
 exports.shorthands = undefined;
 
-exports.up = pgm => {};
+exports.up = pgm => {
+   pgm.sql(`
+      CREATE TABLE comments (
+         id SERIAL PRIMARY KEY,
+         created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+         updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+         contents VARCHAR(240) NOT NULL
+      );
+   `)
+};
 
-exports.down = pgm => {};
+exports.down = pgm => {
+   pgm.sql(`
+      DROP TABLE comments;
+   `);
+};
