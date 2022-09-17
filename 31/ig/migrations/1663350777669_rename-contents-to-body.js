7f276b0fa2e5b3d05545aa52bc60cd5ed33c8966
danielchukwu <00danzy@gmail.com>
2022-09-17 08:03:36 +0100

    first commit

--- 31/ig/migrations/1663350777669_rename-contents-to-body.js
+++ 31/ig/migrations/1663350777669_rename-contents-to-body.js
@@ -4,14 +4,10 @@ exports.shorthands = undefined;
 
 exports.up = pgm => {
    pgm.sql(`
-      ALTER TABLE comments 
-      RENAME COLUMN contents TO body
    `);
 };
 
 exports.down = pgm => {
    pgm.sql(`
-      ALTER TABLE comments
-      RENAME COLUMN body TO contents
    `) 
 };
