7494e93e8b6c899b3aede11df089727dea1e806b
danielchukwu <00danzy@gmail.com>
2022-09-17 08:01:50 +0100

    create comments table in transaction

--- 31/ig/migrations/1663331419191_table-comments.js
+++ 31/ig/migrations/1663331419191_table-comments.js
@@ -4,17 +4,21 @@ exports.shorthands = undefined;
 
 exports.up = pgm => {
    pgm.sql(`
+      BEGIN;
       CREATE TABLE comments (
          id SERIAL PRIMARY KEY,
          created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
          updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
          contents VARCHAR(240) NOT NULL
       );
+      COMMIT;
    `)
 };
 
 exports.down = pgm => {
    pgm.sql(`
+      BEGIN;
       DROP TABLE comments;
+      COMMIT;
    `);
 };
