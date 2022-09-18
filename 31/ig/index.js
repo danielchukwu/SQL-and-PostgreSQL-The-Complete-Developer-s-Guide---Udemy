907cbfd947f7ac52e89cdc2712d6268d6fe8b8a8
danielchukwu <00danzy@gmail.com>
2022-09-18 19:07:36 +0100

    add create functionality

--- 31/ig/index.js
+++ 31/ig/index.js
@@ -16,47 +16,62 @@ const pool = new pg.Pool({
 const app = express();
 app.use(express.urlencoded({ extended : true }));
 
-app.get('/posts', async (req, res) => {
-   const { rows } = await pool.query(`
-      SELECT * FROM posts;
-   `);
+app.route('/posts')
+   .get(async (req, res) => {
+      const { rows } = await pool.query(`
+         SELECT * FROM posts;
+      `); 
 
-   res.send(`
-      <table>
-         <thead>
-            <tr>
-               <th>id</th>
-               <th>lat</th>
-               <th>lng</th>
-            </tr>
-         </thead>
-         <tbody>
-            ${rows.map((row) => {
-               return `
-                  <tr>
-                     <td>${row.id}</td>
-                     <td>${row.lat}</td>
-                     <td>${row.lng}</td>
-                  </tr>
-               `
-            })}
-         </tbody>
-      </table>
+      res.send(`
+         <table>
+            <thead>
+               <tr>
+                  <th>id</th>
+                  <th>lat</th>
+                  <th>lng</th>
+               </tr>
+            </thead>
+            <tbody>
+               ${rows.map((row) => {
+                  if (row.lat !== null)
+                     return `
+                        <tr>
+                           <td>${row.id}</td>
+                           <td>${row.lat}</td>
+                           <td>${row.lng}</td>
+                        </tr>
+                     `
+               })}
+            </tbody>
+         </table>
 
-      <form method="POST">
-         <h3>Create Post</h3>
-         <div>
-            <label>Lat</label>
-            <input type="text" name="lat" />
-         </div>
-         <div>
-            <label>Lng</label>
-            <input type="text" name="lng" />
-         </div>
-         <button type="submit" >Create</button>
-      </form>
-   `);
-});
+         <form method="POST">
+            <h3>Create Post</h3>
+            <div>
+               <label>Lat</label>
+               <input type="text" name="lat" />
+            </div>
+            <div>
+               <label>Lng</label>
+               <input type="text" name="lng" />
+            </div>
+            <button type="submit" >Create</button>
+         </form>
+      `);
+   })
+
+   .post((req, res) => {
+      console.log('Post request recieved...')
+      const lat = req.body.lat
+      const lng = req.body.lng
+      
+      pool.query(`
+         INSERT INTO posts (lat, lng)
+         VALUES (${lat}, ${lng})
+      `)
+
+      res.send('POST CREATED!')
+   })
 
 app.listen(3005, () => {
    console.log('Listening on port 3005');
