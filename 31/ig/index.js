59cd8d25749100756e2a7ddc17f6af52ed5c99cb
danielchukwu <00danzy@gmail.com>
2022-09-18 18:09:04 +0100

    add table structure for posts

--- 31/ig/index.js
+++ 31/ig/index.js
@@ -10,4 +10,34 @@ const pool = new pg.Pool({
    password: '',
 });
 
-pool.query(`SELECT 1+1 AS sum;`).then((res) => console.log(res));
+// pool.query(`SELECT 1+1 AS sum;`).then((res) => console.log(res));
+
+
+app.get('/posts', async (req, res) => {
+   const { rows } = pool.query(`
+      SELECT * FROM posts;
+   `)
+
+   res.send(`
+      <table>
+         <thead>
+            <tr>
+               <th>id</th>
+               <th>lat</th>
+               <th>lng</th>
+            </tr>
+         </thead>
+         <tbody>
+            ${rows.map((row) => {
+               return `
+                  <tr>
+                     <td>${row.id}</td>
+                     <td>${row.lat}</td>
+                     <td>${row.lng}</td>
+                  </tr>
+               `
+            })}
+         </tbody>
+      </table>
+   `)
+})
