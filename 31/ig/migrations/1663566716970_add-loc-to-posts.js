8e316189c2c870e9e93e91f6566caa3e6f823c36
danielchukwu <00danzy@gmail.com>
2022-09-19 06:54:39 +0100

    complete migration

--- 31/ig/migrations/1663566716970_add-loc-to-posts.js
+++ 31/ig/migrations/1663566716970_add-loc-to-posts.js
@@ -3,7 +3,15 @@
 exports.shorthands = undefined;
 
 exports.up = pgm => {
+   pgm.sql(`
+      ALTER TABLE posts
+      ADD COLUMN loc;
+   `)
 };
 
 exports.down = pgm => {
+   pgm.sql(`
+      ALTER TABLE posts
+      DROP COLUMN loc;
+   `)
 };
