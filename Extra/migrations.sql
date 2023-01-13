743957aad1b1dc2f2aec31b07ba2524d30325c91
danielchukwu <00danzy@gmail.com>
2023-01-13 10:53:44 +0100

    first commit

---
+++ Extra/migrations.sql
@@ -0,0 +1,16 @@
+-- TUTORIAL: How to run migrations
+
+-- Using the node-pg-migrate package
+
+-- To run migrations
+
+-- ON GIT BASH TERMINAL (migrations directory)
+-- DATABASE_URL=postgres://USERAME:PASSWORD@localhost:5432/DATABASE_NAME npm run migrate up
+
+-- for windows
+-- USERNAME = postgres
+-- PASSWORD = what_you_used_as_password_when_installing_pg_Admin
+
+
+-- Usecase (windows git bash)
+-- DATABASE_URL=postgres://postgres:alexaislander10@localhost:5432/shopify npm run migrate up
